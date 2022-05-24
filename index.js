97f5db89e474e45498e15fc4422c06a5882d2db5
Luis Corona <luiacorona029@icloud.com>
2022-05-24 16:51:52 -0700

    Changes * index.js (Server is setup and running)

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // import the server and start it!
-console.log('Hello world!')
+const express = require('express')
+const server = express()
+
+server.get('/hello', (req, res) => {
+  res.json({message: 'Hello!'})
+})
+
+server.listen(9000, () => {
+  console.log('\n*** Server is running on port 9000 ***\n')
+} )
